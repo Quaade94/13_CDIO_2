8ba864baa14f7bb5f9946d713b7f3d2d10f62ca1
LukasBek <Lukas@Lukas-PC>
2015-11-05 01:55:59 +0100

    Tilføjet start på testklassen for terning chancer. Resten laves i morgen

--- src/Test/TestAfSpil.java
+++ src/Test/TestAfSpil.java
@@ -8,21 +8,33 @@ import Spil.GUI;
 import Spil.Spiller;
 import Spil.Spilleplade;
 import Spil.Language;
+import Spil.Terninger;
 
 public class TestAfSpil {
 
-	GUI testspil = new GUI();
-	Spiller spil = new Spiller();
-	Spilleplade plade = new Spilleplade();
+	GUI testgame = new GUI();
+	Spiller game = new Spiller();
+	Spilleplade field = new Spilleplade();
 	Language language = new Language();
+	Terninger dice = new Terninger();
+	
+//	@Test
+//	public void testNegativePoint() {
+//		game.setPlayerScorre(true);
+//		field.getFeltEffekt(5);
+//		game.setPlayerScorre(true);
+//		int retur = game.getPlayerScorre(true);
+//		System.out.println("1 "+retur);
+//		System.out.println("2 "+field.getFeltEffekt(5));
+//		
+//	}
 	
 	@Test
-	public void testAfNegativePoint() {
-		plade.getFeltEffekt(5);
-		spil.setPlayerScorre(true);
-		int retur = spil.getPlayerScorre(true);
-		System.out.println("1 "+retur);
-		
+	public void testOccurencesDices(){
+		int diceroll = dice.Roll()[2];
+//		sleep
+		System.out.println("dice "+diceroll);
 	}
 
 }
+// Ronni: test occurenses (dices, fields), ?
