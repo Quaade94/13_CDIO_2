f58c662e312da53d5d354da185cb634e6da9909c
SebbyG <seb.hop@hotmail.com>
2015-11-06 10:50:39 +0100

    Slettet irrelevant kode

--- src/Game/Die.java
+++ src/Game/Die.java
@@ -4,37 +4,11 @@ public class Die {
 
 	private int dice1;
 	private int dice2;
-	private boolean preMode;
-	
-	public void setPreDiceMode (boolean setMode){
-		
-		preMode = setMode;
-		
-	}
-	
-	public void setPreDiceValue (int d1, int d2){
-		
-		dice1 = d1;
-		dice2 = d2;
-		
-	}
 	
 	public int[] roll() {
 
 		int [] roll = {0,0,0};
 
-		if (preMode == true) {
-			
-			roll [0] = dice1;
-			roll [1] = dice2;
-			
-			int sum = dice1 + dice2;
-			
-			roll [2] = sum;
-		}
-		
-		if (preMode == false){
-			
 			dice1 = (int) (Math.random() * 6) + 1;
 			dice2 = (int) (Math.random() * 6) + 1;
 
@@ -43,7 +17,6 @@ public class Die {
 			roll [0] = dice1;
 			roll [1] = dice2;
 			roll [2] = sum;
-		}
 		
 		return roll;
 	}
--- src/Test/GameTest.java
+++ src/Test/GameTest.java
@@ -22,6 +22,7 @@ public class GameTest {
 	
 	
 	}
+}
 //	@Test
 ////	public void testNegativePoint() {
 ////		game.setPlayerScorre(true);
@@ -40,74 +41,8 @@ public class GameTest {
 //		System.out.println("dice "+diceroll);
 //	}
 	
-	@Test 
-	public void testPreDices(){
-		
-		dice.setPreDiceMode(true);
-		dice.setPreDiceValue(4, 4);
-		
-		int[] expected = {4,4,8};
-		
-		int[] actual = dice.roll();
-		
-//		System.out.println(" Værdi 1 " + Forv[0] + " Værdi 2 " + Forv[1] +" Værdi 3 " + Forv[2] + " end");
-//		System.out.println(" Værdi 1 " + Roll[0] + " Værdi 2 " + Roll[1] +" Værdi 3 " + Roll[2] + " end");
 
-		assertEquals(expected[0], actual[0]);
-		assertEquals(expected[1], actual[1]);
-		assertEquals(expected[2], actual[2]);
-		
-		dice.setPreDiceMode(false);
-		
-	}
-	
-	@Test 
-	public void testLandOnField(){
-		
-		
-//		Spiller 1 test:
-		
-		
-		int d1 = 4;
-		int d2 = 5;
-		int expectedNewPlace = d1 + d2;
-		
-		dice.setPreDiceMode(true);
-		dice.setPreDiceValue(d1, d2);
-		
-		int [] roll = dice.roll();
-		
-		game.setPlayerPlace(true, roll[2]);
-		
-//		System.out.println("Nyt spiller sted = " + game.getPlayerPlace(true) + " Forventede sted = " + ForventetNytSted);
-		
-		assertEquals(game.getPlayerPlace(true), expectedNewPlace);
-				
-		dice.setPreDiceMode(false);
-		
 
-//		Spiller 2 test:
-				
-		d1 = 5;
-		d2 = 6;
-		expectedNewPlace = d1 + d2;
-		
-		dice.setPreDiceMode(true);
-		dice.setPreDiceValue(d1, d2);
-		
-		roll = dice.roll();
-		
-		game.setPlayerPlace(false, roll[2]);
-		
-//		System.out.println("Nyt spiller sted = " + game.getPlayerPlace(false) + " Forventede sted = " + ForventetNytSted);
-		
-		assertEquals(game.getPlayerPlace(false), expectedNewPlace);
-				
-		dice.setPreDiceMode(false);
-		
-	}
-		
-}
 // Ronni: test occurenses (dices, fields), ?
 
 
